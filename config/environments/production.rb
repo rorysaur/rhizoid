663060acf01352b95a5f28446cc7ddb8e3076fcd
rorysaur <ou.rory@gmail.com>
2015-01-19 18:41:33 -0800

    fix paperclip env var names

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,9 +3,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['aws_bucket_name'],
-      :access_key_id => ENV['aws_access_key_id'],
-      :secret_access_key => ENV['aws_secret']
+      :bucket => ENV['AWS_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 
